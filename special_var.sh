d199c6619c0290e578dfc6af41d132c4c92434c8
NaveenDunthala <Devopsreddy379@gmail.com>
2025-08-03 18:38:20 +0530

    learing command output

--- special_var.sh
+++ special_var.sh
@@ -6,3 +6,8 @@ echo "script name: $0"
 echo "current working directiry: $PWD"
 echo "How directory of current user: $HOME "
 echo "PID of the script EXCUTING Now: $$"
+sleep 100 &
+
+echo " last processing Background PID is : $!"
+
+date
