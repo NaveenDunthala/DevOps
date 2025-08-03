5b75fd752c350622cd66239d3f961b9990c8b7d5
NaveenDunthala <Devopsreddy379@gmail.com>
2025-08-03 18:34:11 +0530

    learing command output

---
+++ special_var.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "All Variables passed to the script: $@"
+echo " Number of variables passed: $#"
+echo "script name: $0"
+echo "current working directiry: $PWD"
+echo "How directory of current user: $HOME "
+echo "PID of the script EXCUTING Now: $$"
