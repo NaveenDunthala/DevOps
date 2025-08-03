b3f83c2f78439d4648861f1fa2345a241761d028
NaveenDunthala <Devopsreddy379@gmail.com>
2025-08-03 10:05:12 +0530

    learing Arrays

---
+++ Array.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+liked_Games=("Cricket" "vollyball" "football" "kabadi")
+
+echo "frist liked game is ${liked_Games[0]}"
+
+echo "second liked game is ${liked_Games[3]}"
+
+echo "last to like game is ${liked_Games[2]}"
+
+echo "liked games are  $liked_games"
