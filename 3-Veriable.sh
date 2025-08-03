803efa307271b70820a4a3b6c6104a46ca73dfee
NaveenDunthala <Devopsreddy379@gmail.com>
2025-08-03 09:51:10 +0530

    trying to read vales in runtime

---
+++ 3-Veriable.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "Please enter you username "
+
+read USERNAME
+
+echo "Username entered is :$USERNAME"
+
+echo "Please enter your password "
+
+read PASS
+
+echo "your entered password is : $PASS"
