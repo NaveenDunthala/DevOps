b22bba001a0f583500bc5f146fc4786a8fc1be34
NaveenDunthala <Devopsreddy379@gmail.com>
2025-08-03 09:17:19 +0530

    conversation between Naveen and Jonney

---
+++ conversation.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "Naveen : Hi Jonney"
+echo "jonney : iam fine, what about you "
+echo "Naveen : yeah,Good .thanks for asking "
+echo "Jonney : what are you doing Present"
+echo "Naveen : Nothing , searching for Job"
