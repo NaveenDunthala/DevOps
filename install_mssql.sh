b0cf9e1e82d87e46ed23d51d8ad7b6172d5c8a72
NaveenDunthala <Devopsreddy379@gmail.com>
2025-08-03 19:10:36 +0530

    installing Mysqld

--- install_mssql.sh
+++ install_mssql.sh
@@ -1,3 +1,23 @@
 #!/bin/bash
 
-dnf install mysql -y
+#dnf install mysql -y
+
+#check the user has root access or not 
+
+USERID=$(id )
+
+echo "user id is : $USERID"
+
+
+#if root access , processd with the script
+
+#other wise throgh the error
+
+#check alredy installed or not , if installed tell the user
+
+#it is alredy installed
+
+#if not installed , install it 
+
+#check it success or not?
+
