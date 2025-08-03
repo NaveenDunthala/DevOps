d330401ebf57ed922409790cca0818db924ea5c8
NaveenDunthala <Devopsreddy379@gmail.com>
2025-08-03 09:43:17 +0530

    veriables part 2

---
+++ 2-Veriable.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+Person1=$1
+Person2=$2
+
+echo "$Person1 : Hi $Person2"
+echo "$Person2 : iam fine, what about you "
+echo "$Person1 : yeah,Good .thanks for asking "
+echo "$Person2 : what are you doing Present"
+echo "$Person1 : Nothing , searching for Job"
