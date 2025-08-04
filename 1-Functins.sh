df9f76d055e663fb8f5afb5a288efa46f529dc2d
NaveenDunthala <Devopsreddy379@gmail.com>
2025-08-04 10:05:01 +0530

    learing functions

--- 1-Functins.sh
+++ 1-Functins.sh
@@ -1,26 +1,26 @@
 #!/bin/bash
 
-R=$/e[31m
-G=$/e[32m
-Y=$/e[33m
+R="/e[31m"
+G="/e[32m"
+Y="/e[33m"
 
 USERID=$(id -u )
 
 CHECK_ROOT(){
     if [ "$USERID" -eq 0 ];
     then
-        echo -e "$G user is root user, we are prosiding to install" #if root access , processd with the script
+        echo -e "${G} user is root user, we are prosiding to install" #if root access , processd with the script
     else 
-        echo -e "$R user is not a root user, please try with root user"
+        echo -e "{$R} user is not a root user, please try with root user"
         exit 1
     fi  
 }   
 VALIDATE(){
     if [ $1 -ne 0 ];
     then
-        echo -e "$R MySql Not Installed , Please Check "
+        echo -e "{$R} MySql Not Installed , Please Check "
     else 
-        echo -e "$G MYSQL Installed suessfully"
+        echo -e "{$G} MYSQL Installed suessfully"
     fi
 }
 CHECK_ROOT
@@ -28,11 +28,11 @@ dnf list installed  mysql
 
 if [ $? -ne 0 ];  #check alredy installed or not , if installed tell the user
 then
-    echo -e "$Y mysql pakage not installed in this system, we are processding to install "
+    echo -e "{$Y} mysql pakage not installed in this system, we are processding to install "
     dnf install mysql -y
     VALIDATE $?
 
 else 
-    echo -e " $Y git Package alredy installed "  #it is alredy installed
+    echo -e " {$Y} git Package alredy installed "  #it is alredy installed
     
 fi
