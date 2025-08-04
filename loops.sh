1dd24de3cf26a620cb8a3e1ecda561c72a7bbf3d
NaveenDunthala <Devopsreddy379@gmail.com>
2025-08-04 10:45:32 +0530

    learing loops

--- loops.sh
+++ loops.sh
@@ -1,10 +1,15 @@
-#!/bin/bash
+# #!/bin/bash
 
-echo "1 "
-echo "2"
-echo "3 "
-echo "4 "
-echo "5 "
-echo "6"
-echo "7 "
-echo "8"
+for i in 1 2 3 4 5 6 7 8 9
+do 
+    echo " print i "
+done
+
+# echo "1 "
+# echo "2"
+# echo "3 "
+# echo "4 "
+# echo "5 "
+# echo "6"
+# echo "7 "
+# echo "8"
