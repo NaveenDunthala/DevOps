85fac0acc5f9aa8840fc99d808eb19855ec82c32
NaveenDunthala <Devopsreddy379@gmail.com>
2025-08-04 09:39:09 +0530

    learing functions

---
+++ 1-Functins.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+VALIDATE(){
+    if [ $1 -ne 0 ];
+    then
+        echo " MySql Not Installed , Please Check "
+    else 
+        echo " MYSQL Installed suessfully"
+    fi
+}
+
+#check the user has root access or not 
+
+USERID=$(id -u )
+
+if [ "$USERID" -eq 0 ];
+
+then
+    echo "user is root user, we are prosiding to install" #if root access , processd with the script
+else 
+    echo "user is not a root user, please try with root user"
+    
+fi
+
+dnf list installed  mysql 
+
+if [ $? -ne 0 ];  #check alredy installed or not , if installed tell the user
+then
+    echo " mysql pakage not installed in this system, we are processding to install "
+    dnf install mysql -y
+    VALIDATE $?
+
+else 
+    echo " git Package alredy installed "  #it is alredy installed
+    
+fi
+
+VALIDATE $?
