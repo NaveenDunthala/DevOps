b52578250726f61add87117b775fd8b1fb6d7e35
NaveenDunthala <Devopsreddy379@gmail.com>
2025-08-04 12:23:20 +0530

    learing log files

---
+++ redirectors.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+LOGS_FOLDER="/var/log/shell_script"
+SCRIPT_NAME=$(echo "$0" | cut -d "." -f1)
+TIME_STAMP=$(date +%Y-%m-%d-%H-%M-%S)
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME-$TIME_STAMP.log"
+mkdir -p $LOGS_FOLDER
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+USERID=$(id -u )
+
+CHECK_ROOT(){
+    if [ "$USERID" -eq 0 ];
+    then
+        echo -e "${G} user is root user, we are prosiding to install $N" &>>$LOG_FILE#if root access , processd with the script
+    else 
+        echo -e "${R} user is not a root user, please try with root user $N" &>>$LOG_FILE
+        exit 1
+    fi  
+}   
+VALIDATE(){
+    if [ $1 -ne 0 ];
+    then
+        echo -e "$2 is...${R} Failed $N"
+    else 
+        echo -e "$2 is... ${G} Sucess $N" &>>$
+    fi
+}
+CHECK_ROOT
+
+for Package in $@
+do 
+    dnf list installed $Package
+    if [ $? -ne 0 ];  #check alredy installed or not , if installed tell the user
+    then
+        echo -e "${Y} $Package not installed in this system, we are processding to install $N" &>>$LOG_FILE
+        dnf install $Package -y
+        VALIDATE $? "installing $Package" &>>$LOG_FILE
+    else
+        echo -e "${Y} $Package is already installed...nothing to do $N" &>>$LOG_FILE
+    fi
+done
