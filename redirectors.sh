5b73add7283cd0e650c00531717b5cf933170dde
NaveenDunthala <Devopsreddy379@gmail.com>
2025-08-04 13:45:53 +0530

    learing log files

--- redirectors.sh
+++ redirectors.sh
@@ -14,24 +14,24 @@ N="\e[0m"
 USERID=$(id -u )
 
 USAGE() {
-    echo -e "${R} USAGE$N:: sudo sh ($SCRIPT_NAME) pakage1 pakage2 pakage3 " &>>$LOG_FILE
+    echo -e "${R} USAGE$N:: sudo sh ($SCRIPT_NAME) pakage1 pakage2 pakage3 " | tee -a &>>$LOG_FILE
 }
 
 CHECK_ROOT(){
     if [ "$USERID" -eq 0 ];
     then
-        echo -e "${G} user is root user, we are prosiding to install $N" &>>$LOG_FILE#if root access , processd with the script
+        echo -e "${G} user is root user, we are prosiding to install $N" | tee -a &>>$LOG_FILE#if root access , processd with the script
     else 
-        echo -e "${R} user is not a root user, please try with root user $N" &>>$LOG_FILE
+        echo -e "${R} user is not a root user, please try with root user $N" | tee -a  &>>$LOG_FILE
         exit 1
     fi  
 }   
 VALIDATE(){
     if [ $1 -ne 0 ];
     then
-        echo -e "$2 is...${R} Failed $N" &>>$LOG_FILE
+        echo -e "$2 is...${R} Failed $N" | tee -a &>>$LOG_FILE
     else 
-        echo -e "$2 is... ${G} Sucess $N" &>>$LOG_FILE
+        echo -e "$2 is... ${G} Sucess $N" | tee -a &>>$LOG_FILE
     fi
 }
 CHECK_ROOT
@@ -46,10 +46,10 @@ do
     dnf list installed "$Package" 
     if [ $? -ne 0 ];  #check alredy installed or not , if installed tell the user
     then
-        echo -e "${Y} $Package not installed in this system, we are processding to install $N" &>>$LOG_FILE
+        echo -e "${Y} $Package not installed in this system, we are processding to install $N" | tee -a  &>>$LOG_FILE
         dnf install "$Package" -y 
         VALIDATE $? "installing $Package" &>>$LOG_FILE
     else
-        echo -e "${Y} $Package is already installed...nothing to do $N" &>>$LOG_FILE
+        echo -e "${Y} $Package is already installed...nothing to do $N" | tee -a  &>>$LOG_FILE
     fi
 done
