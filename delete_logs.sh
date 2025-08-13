b725d6a9fb169e20d4599be5a981769b984fe439
NaveenDunthala <Devopsreddy379@gmail.com>
2025-08-13 18:21:09 +0530

    delete logs

---
+++ delete_logs.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+Source_Dir=/var/log/Frontend
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+if [ -d $Source_Dir ]
+then 
+    echo -e "$Source_Dir $G exists $N"
+else
+    echo " $Source_Dir $R does not exist $N "
+fi
