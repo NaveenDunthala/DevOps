6e7dd394d60f4331befdc51d7b7abbc190461297
NaveenDunthala <Devopsreddy379@gmail.com>
2025-08-13 22:04:30 +0530

    backup logs

--- Backup.sh
+++ Backup.sh
@@ -3,6 +3,7 @@
 Source_Dir=$1
 DEST_DIR=$2
 DAYS=${3:-14} #if $3 is empty ,then it will take 14days
+TIME_STAMP=$(date +%Y-%m-%d-%H-%M-%S)
 R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
@@ -38,6 +39,22 @@ echo "Files:$FILES"
 if [ ! -z $FILES ]
 then
     echo " Files are found"
+    ZIP_FILE="$DEST_DIR/app_logs-$TIME_STAMP.zip"
+    find ${Source_Dir} -name "*.log" -mtime +14 | zip "$ZIP_FILE" -@
+
+    #check if ZIP file is successfully created or not 
+    if [ -f $ZIP_FILE ]
+    then
+        echo " successfullly zipped files older than $DAYS"
+
+        while IFS=read -r file
+        do
+            echo "Deleting file : $file"
+            rm -rf $file
+        done <<< $FILES
+    else 
+        echo " zipping the files is failed"
+    fi 
 else 
     echo "No Files older than $DAys "
 fi
